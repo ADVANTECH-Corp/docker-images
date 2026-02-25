c62c5077a799286ca1b970e2cd78808e2a582ce2
gino.zheng <gino.zheng@advantech.com>
2026-02-25 11:08:25 +0800

    Add entrypoint to dynamic create user and extract-dtb tool

---
+++ advrisc/u24.04-qcsubv1/entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+UNAME=${UNAME:-adv}
+HOST_UID=${HOST_UID:-1000}
+HOST_GID=${HOST_GID:-1000}
+
+if [ "$HOST_UID" -lt 1000 ] || [ "$HOST_UID" -eq "65534" ]; then
+    echo "WARNING: HOST_UID=$HOST_UID is a system UID, falling back to adv (1000:1000)"
+    UNAME="adv"
+    HOST_UID=1000
+    HOST_GID=1000
+fi
+
+if ! getent group "$HOST_GID" >/dev/null && ! getent group "$UNAME" >/dev/null; then
+    groupadd -g "$HOST_GID" "$UNAME"
+fi
+
+if ! id -u "$UNAME" >/dev/null 2>&1; then
+    useradd -M -d "/home/$UNAME" -u "$HOST_UID" -g "$HOST_GID" -s /bin/bash -G sudo "$UNAME"
+fi
+
+if ! grep -q "^$UNAME " /etc/sudoers; then
+    echo "$UNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+fi
+
+cp -r --update=none /etc/skel/. "/home/$UNAME"/
+
+export HOME=/home/$UNAME
+export USER=$UNAME
+
+BASHRC="/home/$UNAME/.bashrc"
+echo "export HOME=/home/$UNAME" >> "$BASHRC"
+echo "export USER=$UNAME" >> "$BASHRC"
+
+chown "$HOST_UID:$HOST_GID" "/home/$UNAME/".*
+
+cd $HOME
+exec gosu "$UNAME" "$@"
